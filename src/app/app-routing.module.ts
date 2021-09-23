1982b4b1dcdfd7a722ecf5e74f1ae7658ee0cfab
hozana-izadora <izadoraferreir@gmail.com>
2021-09-23 10:41:25 -0300

    First Commit Medicar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ConsultasComponent } from './pages/consultas/consultas.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'consultas',
+    component: ConsultasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule, NbCardModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { ConsultasComponent } from './pages/consultas/consultas.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'corporate' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
