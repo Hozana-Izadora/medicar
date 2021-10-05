312b366471f6bea71c8b9da1da09d16c28ebb9a3
hozana-izadora <izadoraferreir@gmail.com>
2021-10-05 12:04:30 -0300

    header

--- src/app/pages/contas/nova-conta/nova-conta.component.ts
+++ src/app/pages/contas/nova-conta/nova-conta.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router'
+import { Router } from '@angular/router'
 import { NovaContaService } from 'src/app/services/nova-conta.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-nova-conta',
@@ -9,34 +10,43 @@ import { NovaContaService } from 'src/app/services/nova-conta.service';
 })
 export class NovaContaComponent implements OnInit {
   showPassword = true;
-   
+
   nome: string = ''
   email: string = ''
   senha: string = ''
-  confimSenha: string = '' 
+  confimSenha: string = ''
 
   constructor(
     private router: Router,
     private NovaContaService: NovaContaService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
-    
+
   }
-  novaConta(){
-    this.NovaContaService.newUser(this.nome,this.email, this.senha).subscribe(
-      (result: any)=>{
-        if(result){
-          window.localStorage.setItem('token', result.token);
-          window.localStorage.setItem('user', result.username);
-          this.router.navigate(['/consultas']);
+  novaConta() {
+    if (this.nome || this.email || this.senha) {
+      this.NovaContaService.newUser(this.nome, this.email, this.senha).subscribe(
+        (result: any) => {
+          if (result) {
+            window.localStorage.setItem('token', result.token);
+            window.localStorage.setItem('user', result.username);
+            this.router.navigate(['/consultas']);
+          }
         }
-      }
-    )
+      )
+    } else {
+      Swal.fire({
+        title: 'Erro',
+        text: 'Preencha todos os campos!',
+        icon: 'error',
+        confirmButtonText: 'ok'
+      })
+    }
   }
 
-  cancel(){
-    this.router.navigate(["login"])
+  cancel() {
+    this.router.navigate([""])
   }
 
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -41,4 +41,4 @@ export class LoginComponent implements OnInit {
       })
     }
   }
-}
+} 
